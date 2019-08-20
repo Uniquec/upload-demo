adec2f89a1e25b2695010cbf660202c6034b5892
cenhuanya <cenhuanya@jiayouxueba.cn>
2019-08-20 15:22:43 +0800

    简单demo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import {
-  Upload,
-  message,
-  Icon
-} from 'antd';
-
-import styles from './App.less';
+import { Upload, Icon, Modal, message } from 'antd';
+import './App.css';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false,
+      previewVisible: false,
+      previewImage: '',
+      fileList: [],
     }
   }
 
-  // 上传文件之前的钩子，参数为上传的文件，若返回 false 则停止上传。（支持返回Promise对象）
+  getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+
   beforeUpload = (file) => {
     const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
     if (!isJpgOrPng) {
@@ -27,35 +29,48 @@ export default class App extends Component {
     }
     return isJpgOrPng && isLt2M;
   }
-
-  // 上传文件改变时的状态 上传中、完成、失败都会调用这个函数
-  handleChange = info => {
-    if (info.file.status === 'uploading') {
-      this.setState({ loading: true });
-      return;
-    }
-    if (info.file.status === 'done') {
-      // Get this url from response in real world.
-      // getBase64(info.file.originFileObj, imageUrl =>
-      //   this.setState({
-      //     imageUrl,
-      //     loading: false,
-      //   }),
-      // );
-    }
-  };
   
-  render() {
+
+  handleCancel = () => this.setState({ previewVisible: false });
+
+  handlePreview = file => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    });
+  };
+
+  handleChange = info => {
+    this.setState({
+      fileList: info.fileList
+    })
+  };
+
+  render(){
+    const { previewVisible, previewImage, fileList } = this.state;
+    const uploadButton = (
+      <div>
+        <Icon type='plus' />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+
     return (
-      <Upload 
-        listType="picture-card"
-        className={styles.avatarUploader}
-        action=""
-        beforeUpload={this.beforeUpload}
-        onChange={this.handleChange}
-      >
-        <Icon type="plus" />
-      </Upload>
+      <div className="clearfix">
+        <Upload
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          listType="picture-card"
+          fileList={fileList}
+          beforeUpload={this.beforeUpload}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange}
+        >
+          {fileList.length >= 1 ? null : uploadButton}
+        </Upload>
+        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
     );
   }
 }
